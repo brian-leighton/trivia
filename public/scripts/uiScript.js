b0428d7c0cf8c739aec25341d4f2a022e79b05ae
Brian Leighton <bleighton09@gmail.com>
2021-06-27 19:16:45 -0400

    updates

--- public/scripts/uiScript.js
+++ public/scripts/uiScript.js
@@ -1,5 +1,21 @@
 //QUESTION HISTORY SLIDE BAR
 const qHistory = document.querySelector('.quiz__history');
+const qContent = document.querySelector('.quiz__content');
 document.querySelector('.quiz__history--btn').addEventListener('click', () => {
+    qContent.classList.toggle("quiz__content--hide");
     qHistory.classList.toggle('quiz__history--expand');
 });
+let qAnswers = document.querySelectorAll(".quiz__answer--multiple");
+for(let i = 0; i < qAnswers.length; i++){
+    qAnswers[i].addEventListener('click', (e) => selectAnswer(e));
+}
+
+function selectAnswer(event){
+    const selectedAnswers = document.querySelectorAll(".quiz__answer--selected");
+    //clear all instances of selected class;
+    selectedAnswers.forEach(answer => {
+        answer.classList.remove("quiz__answer--selected");
+    })
+    //apply selected class to event target
+    event.target.classList.add("quiz__answer--selected");
+}
